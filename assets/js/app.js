83a0ae8f634d11840a907a892319528c3355d08f
IsraelRuPe <rufino.gabriel420@gmail.com>
2023-08-12 13:57:38 -0600

    modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,30 @@
-// // const id = document.getElementById('id');
-// // const cargo = document.getElementById('cargo');
-// // const employee = document.getElementById('employee');
-// // const namee = document.getElementById('name');
-// // const surnames = document.getElementById('surnames');
-// // const email = document.getElementById('email');
-// // const password = document.getElementById('password');
-// // const number = document.getElementById('number');
-// // const btnUser = document.getElementById('btnUser');
-// // const formUsers = document.getElementById('formUsers');
+const id = document.getElementById('id');
+const position = document.getElementById('position');
+const employee = document.getElementById('employee');
+const namee = document.getElementById('name');
+const surnames = document.getElementById('surnames');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const number = document.getElementById('number');
 
-// // Agregar el evento click al botón para mostrar el modal y limpiar los campos
-// btnUser.addEventListener('click', () => {
-//     // Limpiar los campos del formulario
-//     // id.value = '';
-//     // cargo.value = '';
-//     // employee.value = '';
-//     // namee.value = '';
-//     // surnames.value = '';
-//     // email.value = '';
-//     // password.value = '';
-//     // number.value = '';
-    
-//     // Mostrar el modal utilizando Bootstrap's Modal API
-//     // const myModal = new bootstrap.Modal(document.getElementById('modalUsers'));
-//     // myModal.show();
-// });
+
+const btnUser = document.getElementById('btnUser');
+const formUsers = document.getElementById('formUsers');
+btnUser.addEventListener('click', () => {
+id.value = '';
+position.value = '';
+employee.value = '';
+namee.value = '';
+surnames.value = '';
+email.value = '';
+password.value = '';
+number.value = '';})
 
 document.addEventListener('DOMContentLoaded', function() {
     const btnUser = document.getElementById('btnUser');
 
     btnUser.addEventListener('click', () => {
-        const myModal = new bootstrap.Modal(document.getElementById('modalUsers'));
+        const myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
         myModal.show();
     });
 });
